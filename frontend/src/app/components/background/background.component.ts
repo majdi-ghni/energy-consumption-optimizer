d3ef2a20b5a06ba43335db8c5e32069070f13cca
Muhammad Majdi Abdul Ghni <muhammad-majdi.abdul-ghni@adesso.de>
2023-11-15 13:44:54 +0100

    Add initial components

---
+++ frontend/src/app/components/background/background.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-background',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './background.component.html',
+  styleUrl: './background.component.css'
+})
+export class BackgroundComponent {
+
+}

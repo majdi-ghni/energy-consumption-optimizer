23fbd92d69c8f4e696bd2360ec462dc998ffae34
Muhammad Majdi Abdul Ghni <muhammad-majdi.abdul-ghni@adesso.de>
2023-11-15 15:29:21 +0100

    Set up angular router

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {InputComponent} from "./components/input/input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InputComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RegisterComponent} from "./pages/register/register.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes), RouterModule ],
+})
+export class AppRoutingModule {}

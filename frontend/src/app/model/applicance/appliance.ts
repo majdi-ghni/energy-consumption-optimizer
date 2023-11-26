7a73fcd7a62aeccf6dc3fc710df7d7fa6ccb92e1
Muhammad Majdi Abdul Ghni <muhammad-majdi.abdul-ghni@adesso.de>
2023-11-26 11:33:38 +0100

    Delete unneeded attribute from appliance

--- frontend/src/app/model/applicance/appliance.ts
+++ frontend/src/app/model/applicance/appliance.ts
@@ -3,7 +3,6 @@ import { ApplianceUsageType } from '../appliance-usage-type/applianceUsageType';
 export interface Appliance {
   id: string;
   name: string;
-  model: string;
   estimatedUsageDuration: number; //duration in minutes
   powerRating: number; //amount of electrical power the appliance consumes in kilowatts
   applianceUsageType: ApplianceUsageType;
